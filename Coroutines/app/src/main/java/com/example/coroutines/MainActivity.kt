b9027e41dca0caa273c58f6b5daaed7ca9608e19
An Nguyen <annd2003.vn@gmail.com>
2024-01-10 15:20:06 +0700

    done homework Coroutines

---
+++ Coroutines/app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isRunningPlus = true
+    private var isRunningMinus = true
+    private var isRunningMultiply = true
+    private var isRunningRandom = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // phep cong
+        binding.btStopAddition.setOnClickListener() {
+            isRunningPlus = !isRunningPlus
+            binding.btStopAddition.text = if (isRunningPlus) "Dừng cộng" else "Chạy lại cộng"
+        }
+        startPlusGeneration()
+
+        // phep tru
+        binding.btStopMinus.setOnClickListener() {
+            isRunningMinus = !isRunningMinus
+            binding.btStopMinus.text = if (isRunningMinus) "Dừng trừ" else "Chạy lại trừ"
+        }
+        startMinusGeneration()
+
+        // phep nhan
+        binding.btStopKernel.setOnClickListener() {
+            isRunningMultiply = !isRunningMultiply
+            binding.btStopKernel.text = if (isRunningMultiply) "Dừng nhân" else "Chạy lại nhân"
+        }
+        startMultiplyGeneration()
+
+        // random
+        binding.btStopRandom.setOnClickListener() {
+            isRunningRandom = !isRunningRandom
+            binding.btStopRandom.text = if (isRunningRandom) "Dừng Random" else "Chạy lại Random"
+        }
+        startRandomGeneration()
+
+    }
+
+    private fun startPlusGeneration() {
+        GlobalScope.launch(Dispatchers.Main) {
+            var plus = 0
+            while (isActive) {
+                if (isRunningPlus) {
+                    plus++
+                    binding.tvValuePlus.text = "Giá trị hiện tại là: $plus"
+                }
+                delay(1000)
+            }
+        }
+    }
+
+    private fun startMinusGeneration() {
+        GlobalScope.launch(Dispatchers.Main) {
+            var minus = 0
+            while (isActive) {
+                if (isRunningMinus) {
+                    --minus
+                    binding.tvValueMinus.text = "Giá trị hiện tại là: $minus"
+                }
+                delay(1000)
+            }
+        }
+    }
+
+    private fun startMultiplyGeneration() {
+        GlobalScope.launch(Dispatchers.Main) {
+            var multiply = 1
+            while (isActive) {
+                if (isRunningMultiply) {
+                    multiply *= 2
+                    binding.tvValueMultiply.text = "Giá trị hiện tại là: $multiply"
+                }
+                delay(5000)
+            }
+        }
+    }
+
+    private fun startRandomGeneration() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val random = Random
+            while (isActive) { //kiem tra trang thai cua coroutine
+                if (isRunningRandom) {
+                    val randomValue = random.nextInt()
+                    binding.tvValueRandom.text = "Giá trị hiện tại là: $randomValue"
+                }
+                delay(3000)
+            }
+        }
+    }
+}
